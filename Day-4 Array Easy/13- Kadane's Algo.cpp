11a7452a929c8b5b9e79b4b4877df6fad52d7f09
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-09-30 23:59:48 +0530

    Create 13- Kadane's Algo.cpp

---
+++ Day-4 Array Easy/13- Kadane's Algo.cpp
@@ -0,0 +1,30 @@
+//   https://leetcode.com/problems/maximum-subarray/description/
+
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int sum = 0;
+        int max_sum = INT_MIN;
+
+
+        for(int i=0;i<n;i++){
+
+            sum += nums[i];
+
+            max_sum = max(max_sum, sum);
+            
+            if(sum<0){
+                sum = 0;
+            }
+            
+
+        }
+
+        return max_sum;
+    }
+};
