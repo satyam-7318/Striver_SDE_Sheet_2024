000b18ebd5336b9e9cf8d5c857e4474ee14a6c67
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-10-03 23:57:37 +0530

    Create 2- Majority Element.cpp

---
+++ Day-5 Array Medium/2- Majority Element.cpp
@@ -0,0 +1,33 @@
+//  https://leetcode.com/problems/majority-element/description/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+
+        // Can do using hashmap as well
+
+        // 2nd method - Moore Voting Algo
+
+        int count = 0;
+        int x = -1;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            
+            if(count==0){
+                x = nums[i];
+            }
+            
+            if(x==nums[i])
+                count++;
+            else
+                count--;
+            
+        }
+        
+        return x;
+
+    }
+};
