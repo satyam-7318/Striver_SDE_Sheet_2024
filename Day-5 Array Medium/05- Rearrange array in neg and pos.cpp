f506a8aace9d4e23e069da8b2dc19c339344cb8a
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-10-10 23:36:30 +0530

    Create 05- Rearrange array in neg and pos.cpp

---
+++ Day-5 Array Medium/05- Rearrange array in neg and pos.cpp
@@ -0,0 +1,29 @@
+//   https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        
+        int pos=0, neg=1;
+        int n = nums.size();
+
+        vector<int> ans(n,0);
+
+        for(int i=0;i<n;i++){
+
+            if(nums[i] > 0){
+                ans[pos] = nums[i];
+                pos = pos + 2;
+            }
+            else{
+                ans[neg] = nums[i];
+                neg = neg + 2;
+            }
+
+
+        }
+
+        return ans;
+
+    }
+};
