6548352cd06cb2f32e3acc6846a947fea4d0e046
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-11-02 20:41:22 +0530

    Create 02- Floor in sorted array.cpp

---
+++ Day-7 Binary Search 1D Array/02- Floor in sorted array.cpp
@@ -0,0 +1,63 @@
+// https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    int findFloor(vector<int>& arr, int k) {
+
+        // Your code here
+        int low = 0;
+        int high = arr.size() - 1;
+        
+        int ans = -1;
+        
+        while(low <= high){
+            
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] <= k){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        int ans = ob.findFloor(arr, k);
+
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
