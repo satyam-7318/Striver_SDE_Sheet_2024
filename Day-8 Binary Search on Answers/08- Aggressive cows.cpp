7afd8b63381ebec29085d7f7a594adbeb44c1cb2
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-12-04 15:43:39 +0530

    Create 08- Aggressive cows.cpp

---
+++ Day-8 Binary Search on Answers/08- Aggressive cows.cpp
@@ -0,0 +1,33 @@
+// https://www.spoj.com/problems/AGGRCOW/
+
+
+bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1, high = stalls[n - 1] - stalls[0], ans = high;
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (canWePlace(stalls, mid, k) == true) {
+            ans = mid;
+            low = mid + 1;
+        }
+        else high = mid - 1;
+    }
+    return ans;
+}
